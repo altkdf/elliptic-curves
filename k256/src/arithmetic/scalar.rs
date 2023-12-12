b18312f214af8d4e91a756458e8acab0009e7eaa
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2023-12-12 09:31:13 +0000

    line

--- k256/src/arithmetic/scalar.rs
+++ k256/src/arithmetic/scalar.rs
@@ -677,7 +677,8 @@ impl Reduce<U256> for Scalar {
 
 impl Reduce<U512> for Scalar {
     type Bytes = WideBytes;
-
+    
+    #[inline(always)]
     fn reduce(w: U512) -> Self {
         WideScalar(w).reduce()
     }
