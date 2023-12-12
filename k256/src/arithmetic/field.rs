9137e6dc104fd69559062bbbabac8f5161ac136c
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2023-12-12 09:31:59 +0000

    line

--- k256/src/arithmetic/field.rs
+++ k256/src/arithmetic/field.rs
@@ -391,6 +391,7 @@ impl Add<&FieldElement> for FieldElement {
 impl Add<&FieldElement> for &FieldElement {
     type Output = FieldElement;
 
+    #[inline(always)]
     fn add(self, other: &FieldElement) -> FieldElement {
         FieldElement(self.0.add(&(other.0)))
     }
