97d80f4db811965f1fe3de696147c7e3cc3fed78
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2023-12-22 11:29:50 +0000

    line

--- p256/src/arithmetic/field.rs
+++ p256/src/arithmetic/field.rs
@@ -632,6 +632,7 @@ impl Mul<&FieldElement> for FieldElement {
 impl Mul<&FieldElement> for &FieldElement {
     type Output = FieldElement;
 
+    #[inline(always)]
     fn mul(self, other: &FieldElement) -> FieldElement {
         FieldElement::multiply(self, other)
     }
